2123557d947fa62fd7cc1df679bb6728182d1a72
Amanda Maring <amaring@users.noreply.github.com>
2018-08-06 22:39:10 -0400

    add header & footer - removed hardcoded db connection

--- add_topic.php
+++ add_topic.php
@@ -1,14 +1,8 @@
 <?php
 
-$host="localhost"; // Host name
-$username="DFadmin"; // Mysql username
-$password="database_password"; // Mysql password
-$db_name="DBLR"; // Database name
+include_once('templates/header.php');
+include('config/config.php');
 $tbl_name="forum_question"; // Table name
-
-// Connect to server and select database.
-$link=mysqli_connect("$host", "$username", "$password")or die("cannot connect");
-mysqli_select_db($link, "$db_name")or die("cannot select DB");
 
 // get data that sent from form
 $topic=$_POST['topic'];
@@ -19,7 +13,7 @@ $email=$_POST['email'];
 $datetime=date("d/m/y h:i:s"); //create date time
 
 $sql="INSERT INTO $tbl_name(topic, detail, name, email, datetime)VALUES('$topic', '$detail', '$name', '$email', '$datetime')";
-$result=mysqli_query($link, $sql);
+$result = mysqli_query($mysqli, $sql) or die('-1'.mysqli_error());
 
 if($result){
 echo "Successful<BR>";
@@ -28,5 +22,7 @@ echo "<a href=forum.php>View your topic</a>";
 else {
 echo "ERROR";
 }
-mysqli_close();
+//mysqli_close();
+
+include_once('templates/footer.php');
 ?>
