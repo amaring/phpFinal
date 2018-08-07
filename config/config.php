09d67c47a3353ae98d9df22d8e2e5a78a3360861
Amanda Maring <amaring@users.noreply.github.com>
2018-08-06 21:55:56 -0400

    changed config.php for more local database pulls

--- config/config.php
+++ config/config.php
@@ -2,12 +2,24 @@
 /*
   This file contains often used constants
 */
-  // DB Params
-  define("DB_HOST", "localhost");
-  define("DB_USER", "DFadmin");
-  define("DB_PASS", "database_password");
-  define("DB_NAME", "DBLR");
+  // DB Params - Scrubbed here
+	$dbhost = 'localhost';
+	$dbuser = 'username';
+	$dbpass = 'password';
+	$dbname = 'database';
 
+	//Connect
+	$mysqli = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+if (mysqli_connect_errno()) {
+	printf("MySQLi connection failed: ", mysqli_connect_error());
+	exit();
+}
+
+// Change character set to utf8
+if (!$mysqli->set_charset('utf8')) {
+	printf('Error loading character set utf8: %s\n', $mysqli->error);
+}
+  
   // App Root
   define('APPROOT', dirname(dirname(__FILE__)));
   // URL Root (eg. http://myapp.com or http://localhost/myapp)
@@ -15,4 +27,4 @@
   // Site Name
   define('SITENAME', 'OUR_SITE_NAME');
   
-  
+ ?>
