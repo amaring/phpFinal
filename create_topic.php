7a941efed05bae3f6ca8d937bdcaa862ebdb0b1a
Amanda Maring <amaring@users.noreply.github.com>
2018-08-07 20:33:54 -0400

    refactor to change how the form appears

--- create_topic.php
+++ create_topic.php
@@ -1,45 +1,18 @@
 <?php
-include('config/config.php');
 include_once('templates/header.php');
 ?>
-<table width="400" border="0" align="center" cellpadding="0" cellspacing="1" bgcolor="#CCCCCC">
-	<tr>
-<form id="form1" name="form1" method="post" action="add_topic.php">
-	<td>
-<table width="100%" border="0" cellpadding="3" cellspacing="1" bgcolor="#FFFFFF">
-	<tr>
-	<td colspan="3" bgcolor="#E6E6E6"><strong>Create New Topic</strong> </td>
-	</tr>
-	<tr>
-	<td width="14%"><strong>Topic</strong></td>
-	<td width="2%">:</td>
-	<td width="84%"><input name="topic" type="text" id="topic" size="50" /></td>
-	</tr>
-	<tr>
-	<td valign="top"><strong>Detail</strong></td>
-	<td valign="top">:</td>
-	<td><textarea name="detail" cols="50" rows="3" id="detail"></textarea></td>
-	</tr>
-	<tr>
-	<td><strong>Name</strong></td>
-	<td>:</td>
-	<td><input name="name" type="text" id="name" size="50" /></td>
-	</tr>
-	<tr>
-	<td><strong>Email</strong></td>
-	<td>:</td>
-	<td><input name="email" type="text" id="email" size="50" /></td>
-	</tr>
-	<tr>
-	<td>&nbsp;</td>
-	<td>&nbsp;</td>
-	<td><input type="submit" name="Submit" value="Submit" /> <input type="reset" name="Submit2" value="Reset" /></td>
-	</tr>
-</table>
-	</td>
-		</form>
-	</tr>
-</table>
+<form class="reply_form" id="form1" name="form1" method="post" action="add_topic.php">
+<h1>Create New Topic</h1>
+<label for="topic">Topic</label>
+<input name="topic" type="text" id="topic" /><br />
+<label for="detail">Detail</label>
+<textarea name="detail" d="detail"></textarea><br />
+<label for="name">Name</label>
+<input name="name" type="text" id="name" /><br />
+<label for="email">Email</label>
+<input name="email" type="text" id="email" /><br />
+<input type="submit" name="Submit" value="Submit" /> <input type="reset" name="Submit2" value="Reset" /></td>
+</form>
 <?php
 include_once('templates/footer.php');
 ?>
